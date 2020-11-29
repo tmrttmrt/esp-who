71c1776879a3440efcad0d81d8cca82a66758a0f
Tomaz Mertelj <tmertelj@gmail.com>
2020-11-30 00:03:55 +0100

    Ota works

---
+++ examples/single_chip/just_ota/main/app_otau.c
@@ -0,0 +1,12 @@
+#include "app_otau.h"
+
+static void ota_server_task(void * param)
+{
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    ota_server_start();
+    vTaskDelete(NULL);
+}
+
+void app_otau_main(){
+    xTaskCreate(&ota_server_task, "ota_server_task", 4096, NULL, 5, NULL);
+}
---
+++ examples/single_chip/just_ota/main/include/app_otau.h
@@ -0,0 +1,45 @@
+/*
+  * ESPRESSIF MIT License
+  *
+  * Copyright (c) 2017 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+  *
+  * Permission is hereby granted for use on ESPRESSIF SYSTEMS products only, in which case,
+  * it is free of charge, to any person obtaining a copy of this software and associated
+  * documentation files (the "Software"), to deal in the Software without restriction, including
+  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+  * to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all copies or
+  * substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  *
+  */
+#ifndef _APP_OTAU_H_
+#define _APP_OTAU_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "ota_server.h"
+
+#define CONNECTED_BIT BIT0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern EventGroupHandle_t wifi_event_group;
+
+void app_otau_main();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
